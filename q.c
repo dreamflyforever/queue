eb1cfdaf0819095b49b7af561eae90ca96a9d86f
dreamflyforever <sjyangv0@qq.com>
2018-03-27 15:28:04 +0800

    logic run success
    Signed-off-by: dreamflyforever <sjyangv0@qq.com>

--- q.c
+++ q.c
@@ -1,7 +1,144 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <list.h>
+#include <q.h>
+
+U8 msg_init(queue_t **obj, char *name, int count)
+{
+	*obj = malloc(sizeof(queue_t));
+	list_init(&(*obj)->head);
+	(*obj)->name = name;
+	(*obj)->count = count;
+	(*obj)->index = 0;
+	return 0;
+}
+
+U8 msg_deinit(queue_t *obj)
+{
+	free(obj);
+	return 0;
+}
+
+msg_t *msg_pack(char *buf, U8 size)
+{
+	if (buf == NULL) {
+		printf("buf == NULL\n");
+	}
+	msg_t *msg = malloc(sizeof(msg_t));;
+	if (msg == NULL) {
+		goto end;
+	}
+	msg->buf = malloc(size);
+	memcpy(msg->buf, buf, size);
+	msg->length = size;
+	msg->type = 4;
+end:
+	return msg;
+}
+
+U8 msg_depack(msg_t *msg)
+{
+	U8 retval = 0;
+	if (msg == NULL) {
+		printf("msg == NULL\n");
+		retval = -1;
+	}
+	free(msg->buf);
+	free(msg);
+	return retval;
+}
+
+U8 msg_put(queue_t *obj, msg_t *msg)
+{
+	U8 retval = 0;
+	if (obj == NULL) {
+		printf("obj == NULL\n");
+		retval = -1;
+	}
+	list_insert_behind(&obj->head, &msg->list);
+	obj->index++;
+	return retval;
+}
+
+msg_t *msg_get(queue_t *obj)
+{
+	msg_t *msg = NULL;
+	if (obj == NULL) {
+		printf("obj == NULL\n");
+		goto end;
+	}
+	LIST *tmp = NULL;
+	tmp = &(obj->head);
+	while (!is_list_last(tmp)) {
+		msg = list_entry(tmp->next, msg_t, list);
+		print("msg buf: %s\n", msg->buf);
+		list_delete(tmp->next);
+		break;
+	}
+	/*else {
+		retval = -1;
+	}*/
+end:
+	return msg;
+}
+
+U8 msg_put_buf(queue_t *obj, char *buf, U8 size)
+{
+	int retval = 0;
+	msg_t *msg = msg_pack(buf, size);
+	if (msg == NULL) {
+		retval = -1;
+		goto end;
+	}
+	retval = msg_put(obj, msg);
+end:
+	return retval;
+}
+
+U8 msg_get_buf(queue_t *obj, char *buf, U8 size)
+{
+	int retval = 0;
+	int len;
+	msg_t *msg;
+	msg = msg_get(obj);
+	if (msg == NULL) {
+		retval = -1;
+		goto end;
+	}
+	print("msg buf: %s\n", msg->buf);
+
+	if (size > msg->length) len = msg->length;
+	else len = size;
+
+	memcpy(buf, msg->buf, len);
+	print("%s\n", buf);
+	msg_depack(msg);
+end:
+	return retval;
+}
 
 int main()
 {
+	queue_t *obj;
+	msg_init(&obj, "helloworld", 100);
+	msg_put_buf(obj, "1", 1);
+	msg_put_buf(obj, "2", 1);
+	msg_put_buf(obj, "3", 1);
+	msg_put_buf(obj, "4", 1);
+	msg_put_buf(obj, "5", 1);
+	msg_put_buf(obj, "6", 1);
+
+	char buf[1] = {0};
+	msg_get_buf(obj, buf, 1);
+	printf("%s\n", buf);
+	msg_get_buf(obj, buf, 1);
+	printf("%s\n", buf);
+	msg_get_buf(obj, buf, 1);
+	msg_get_buf(obj, buf, 1);
+	msg_get_buf(obj, buf, 1);
+	msg_get_buf(obj, buf, 1);
+	msg_get_buf(obj, buf, 1);
+	msg_deinit(obj);
 	return 0;
 }
--- q.h
+++ q.h
@@ -1,18 +1,35 @@
+#ifndef __Q_H__
+#define __Q_H__
+
 typedef struct queue_str {
-	LIST list;
 	LIST head;
 	U8 count;
 	U8 *name;
+	U8 index;
 } queue_t;
 
 typedef struct msg_str {
 	LIST list;
 	U8 type;
 	U32 length;
-	U8 *buff;
+	U8 *buf;
 } msg_t;
 
-u8 msg_init(queue_t *obj, char *name, int count);
-u8 msg_put(queue_t *obj, msg_t *msg, u8 size);
-u8 msg_get(queue_t *obj, msg_t *msg, u8 size);
-u8 msg_deinit(queue_t *obj);
+U8 msg_init(queue_t **obj, char *name, int count);
+U8 msg_put(queue_t *obj, msg_t *msg);
+msg_t *msg_get(queue_t *obj);
+U8 msg_deinit(queue_t *obj);
+
+msg_t *msg_pack(char *buf, U8 size);
+U8 msg_depack(msg_t *msg);
+
+#if 0
+#define print(format, ...) \
+	{printf("[%s : %s : %d] ", \
+	__FILE__, __func__, __LINE__); \
+	printf(format, ##__VA_ARGS__);}
+#else
+#define print(format, ...) 
+#endif
+
+#endif
